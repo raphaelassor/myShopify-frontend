dd53e06134aee43b67bc650002422aff3cfd4b52
RaphaelAssor <raphaelassor@rabcommerce.com>
2021-08-16 17:01:52 +0300

    merge

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1>Quick Setup</h1>
+
+```
+git clone https://github.com/raphaelassor/myShopify-frontend.git
+```
+
+2. Enter the backend folder and make sure you have node_modules installed. After that we will initiate the server with 'npm start' 
+
+```
+cd myShopify-frontend
+npm i 
+npm start
+```
+You shuold get a console ouput that the server is up and running at port 3030
--- src/services/settings.js
+++ src/services/settings.js
@@ -1,19 +1,22 @@
-export const statusNames={
-    archive:'archive',
-    draft:'draft',
-    active:'active',
+export const statusNames = {
+    archive: 'archive',
+    draft: 'draft',
+    active: 'active',
 }
 
-export const PRODUCT_TYPE='PRODUCT'
-export const ORDER_TYPE='ORDER'
-export const CUSTOMER_TYPE='COLLECTION'
+export const PRODUCT_TYPE = 'PRODUCT'
+export const ORDER_TYPE = 'ORDER'
+export const CUSTOMER_TYPE = 'COLLECTION'
 
 
 //MODAL NAMES
 
-export const TAGS_EDIT_MODAL='EDIT_TAGS'
-export const SINGLE_SELECT_POPOVER='SINGLE_SELECT'
+export const TAGS_EDIT_MODAL = 'EDIT_TAGS'
+export const SINGLE_SELECT_POPOVER = 'SINGLE_SELECT'
 
 
 export const ADD_TAGS_MODE = 'ADD_TAGS';
 export const REMOVE_TAGS_MODE = 'REMOVE_TAGS';
+
+//CONFIGURABLE DATA
+
